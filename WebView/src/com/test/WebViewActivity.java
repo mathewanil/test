f72341612c2fd7ac33a7d8f4372eb2941178da3f
amathew <amathew@dhcp-43-191-65-207.am.sony.com>
2012-02-29 15:38:59 -0800

    new

---
+++ WebView/src/com/test/WebViewActivity.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        WebView.enablePlatformNotifications();
+        
+        setContentView(R.layout.webview_1);
+        
+        final String mimeType = "text/html";
+        final String encoding = "utf-8";
+        
+        WebView wv;
+        
+        wv = (WebView) findViewById(R.id.wv1);
+        //wv.loadData("<a href='x'>Hello World! - 1</a>", mimeType, encoding);
+        //wv.loadUrl("http://vexflow.com/");
+        wv.loadUrl("http://www.google.com");
+        
+        
+    }
+}
