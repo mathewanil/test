7590ed66fcf249177b48279a653c186cf730d95c
amathew <amathew@dhcp-43-191-65-207.am.sony.com>
2012-03-15 16:55:08 -0700

    new

---
+++ online/src/com/online/OnlineActivity.java
@@ -0,0 +1,14 @@
+package com.online;
+
+import com.online.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OnlineActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
